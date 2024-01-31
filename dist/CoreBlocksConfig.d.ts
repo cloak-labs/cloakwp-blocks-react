1708243e38e857c70d44ff4afe1a9db2f15f9e8d
Kaelan Smith <kaelancsmith@gmail.com>
2024-01-31 12:25:48 -0800

    build package - forgot

--- dist/CoreBlocksConfig.d.ts
+++
@@ -1,2 +0,0 @@
-import type { WPBlocksConfigReact } from "@cloakwp/react";
-export declare const CoreBlocksConfig: WPBlocksConfigReact;
--- dist/blocks/button.d.ts
+++ dist/blocks/button.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="react" />
 export declare const button: (userOverrides?: Partial<{
-    variantsRouter: (block: import("cloakwp").BlockDataWithExtraContext<import("react").ComponentClass<any, any> | import("react").FunctionComponent<any>, import("cloakwp").RestApiBlockData>) => "link" | "default";
+    variantsRouter: (block: import("cloakwp").BlockDataWithExtraContext<import("react").ComponentClass<any, any> | import("react").FunctionComponent<any>, import("cloakwp").RestApiBlockData>) => "default" | "link";
     variants: {
         default: {
             dataRouter: import("cloakwp").WPDataRouter;
